a2b468f337a6d0e376c8f97caeac908e859408a0
Maximiliano Gismondi <maximiliano.gismondi@gmail.com>
2018-08-27 00:10:02 -0300

    probando heroku

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
 
-const obj = { message:'Hello World!' }
+const app = express();
+const obj = { message:'Hello World!' };
+const port = process.env.PORT || 5000;
 
-app.get('/hello', (req, res) => res.send(obj))
+app.get('/hello', (req, res) => res.send(obj));
 
-app.listen(5000, () => console.log('Example app listening on port 5000!'))
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
