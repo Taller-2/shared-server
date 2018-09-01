fa738b761311fa882673880661804d7038924485
Dylan Alvarez <helllo5d123@gmail.com>
2018-08-31 23:34:10 -0300

    Revert "Rebuild app based on create-react-app"
    This reverts commit c269e38c0c5dc2dd8323dcc5fe7733a8e02c8647.

---
+++ test/test_sample.js
@@ -0,0 +1,9 @@
+const chai = require("chai");
+const assert = chai.assert;
+
+describe('Sample test suite', () => {
+    it("Sample test", () => {
+        const one = 1;
+        assert.equal(one, 1);
+    });
+});
