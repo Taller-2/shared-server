31b230abdb5c33faa65ab5aa86d8deb5a4f84ab9
Dylan Alvarez <helllo5d123@gmail.com>
2018-09-02 19:30:24 -0300

    Make index.js a bit more clear

--- server/index.js
+++ server/index.js
@@ -5,13 +5,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 // Request body-parsing middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Routes
 app.use('/user', require('./routes/user'));
 app.use('/hello', require('./routes/hello'));
 
+
+// Production-specific setup
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, '../client/build')));
