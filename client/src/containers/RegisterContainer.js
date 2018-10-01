112df7c3054eb9a0dc9485e50c5b646e7b59454c
Maximiliano Gismondi <maximiliano.gismondi@gmail.com>
2018-09-30 21:43:34 -0300

    Mensajes de error en registro de usuarios

--- client/src/containers/RegisterContainer.js
+++ client/src/containers/RegisterContainer.js
@@ -7,34 +7,42 @@ export default class RegisterContainer extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      redirectToLogin: false
+      redirectToLogin: false,
+      errors: {}
     }
   }
 
-  handleClick (name, email, pass) {
-    Http.post('user/', { name, email, pass })
-      .then(response => {
-        if (response.status === 201) {
-          alert('usuario creado exitosamente')
-          this.setState({ redirectToLogin: true })
-        } else {
-          alert('Error al crear usuario') // TODO hacer algo
-        }
-      })
-      .catch(err => {
-        alert('Error al crear usuario' + err) // TODO hacer algo
-      })
+  handleClick (name, email, pass, passConfirmation) {
+    if (pass.localeCompare(passConfirmation)) {
+      this.setState({ errors: { passConfirmation: 'Las contraseñas no coinciden' } })
+    } else {
+      Http.post('user/', { name, email, pass })
+        .then(response => {
+          if (response.status === 201) {
+            alert('Usuario creado exitosamente!')
+            this.setState({ redirectToLogin: true })
+          } else {
+            let errors = {}
+            console.log(response)
+            response.content.forEach(e => { errors[e.param] = e.msg })
+            this.setState({ errors: errors })
+          }
+        })
+        .catch(err => {
+          alert('Error al crear usuario' + err) // TODO hacer algo
+        })
+    }
   }
 
   render () {
-    const { redirectToLogin } = this.state
-
+    const { redirectToLogin, errors } = this.state
+    console.log(errors)
     if (redirectToLogin) {
       return <Redirect to='/' />
     }
 
     return (
-      <RegisterForm onClick={(name, email, pass) => this.handleClick(name, email, pass)} />
+      <RegisterForm errors={errors} onClick={(name, email, pass, passConfirmation) => this.handleClick(name, email, pass, passConfirmation)} />
     )
   }
 }
