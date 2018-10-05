49e430999c4e9ebc6926bb471ccf362874ca9c5b
sebastian <sebastian.e.blanco@gmail.com>
2018-10-05 14:25:31 -0300

    change shipment-cost end point to be able to receive facts throuhg request and apply them to a rule

--- server/routes/shipment_cost.js
+++ server/routes/shipment_cost.js
@@ -1,6 +1,19 @@
 const router = require('express').Router()
 const bodyParser = require('body-parser')
-const engine = require('../rules/shipment_cost_rules')
+// const addRules = require('../rules/shipment_cost_rules')
+// var model = require('../controllers/rules')
+let Rule = require('json-rules-engine').Rule
+let Engine = require('json-rules-engine').Engine
+
+function runRules (engine, facts, res) {
+  let array
+  engine.run(facts).then(triggeredEvents => {
+    // engine returns a list of events with truthy conditions
+    array = triggeredEvents.map(
+      event => ({ message: event.params.data }))
+    res.send(array)
+  }).catch(() => res.send({ message: 'test_rule failed' }))
+}
 
 router.post('/', bodyParser.json(), function (req, res) {
   /*
@@ -27,14 +40,28 @@ router.post('/', bodyParser.json(), function (req, res) {
     tripTime // (string: HH:MM)
   } = req.body
   */
-  let facts = { test_rule: true }
-  let array
-  engine.run(facts).then(triggeredEvents => {
-    // engine returns a list of events with truthy conditions
-    array = triggeredEvents.map(
-      event => ({ message: event.params.data }))
-    res.send(array)
-  }).catch(() => res.send({ message: 'test_rule failed' }))
+  let rule1 = {
+    'conditions': {
+      'all': [{
+        'fact': 'test_rule',
+        'operator': 'equal',
+        'value': true
+      }]
+    },
+    'event': {
+      'type': 'test',
+      'params': {
+        'data': 0
+      }
+    }
+  }
+  // let response
+  // model.findAll(response)
+  // let jsonRules = response.rules
+  let engine = new Engine()
+  engine.addRule(new Rule(rule1)) // addRules(jsonRules)
+  let facts = req.body
+  runRules(engine, facts, res)
 })
 
 module.exports = router
