49a1cddaa2b931e45080e02f95c5b386ecd5abde
sebastian <sebastian.e.blanco@gmail.com>
2018-10-05 14:30:31 -0300

    add rule unit test that post a rule in the data base

---
+++ test/addRuleTest.js
@@ -0,0 +1,60 @@
+const chai = require('chai')
+const chaihttp = require('chai-http')
+var should = require('should')
+var app = require('../server/index')
+var server = app.listen()
+chai.use(chaihttp)
+
+let rule = {
+  'conditions': {
+    'all': [{
+      'fact': 'test_rule',
+      'operator': 'equal',
+      'value': true
+    }]
+  },
+  'event': {
+    'type': 'test',
+    'params': {
+      'data': 0
+    }
+  }
+}
+
+describe('add simple rule', function () {
+  before(function (done) {
+    chai.request(server)
+      .delete('/rules')
+      .end(function (err, res) {
+        // expected: { success: true, rule: rule }
+        should.equal(err, null)
+        res.should.have.status(201)
+        res.body.should.be.a('object')
+        res.body.should.have.property('success')
+        res.body.success.should.be.equal(true)
+        setImmediate(done)
+      })
+  })
+  it('should save a rule in data base and receive success message', function (done) {
+    let jsonRule = JSON.stringify(rule)
+    chai.request(server)
+      .post('/rules')
+      .send({ json: jsonRule })
+      .end(function (err, res) {
+        // expected: { success: true, rule: rule }
+        should.equal(err, null)
+        res.should.have.status(201)
+        res.body.should.be.a('object')
+        res.body.should.have.property('success')
+        res.body.success.should.be.equal(true)
+        res.body.should.have.property('rule')
+        res.body.rule.should.have.property('json')
+        res.body.rule.json.should.equal(jsonRule)
+        setImmediate(done)
+      })
+  })
+  after(function (done) {
+    server.close()
+    done()
+  })
+})
