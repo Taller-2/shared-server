8d3f473c9fd2bfeb0b3b345d2a6b94e276278667
sebastian <sebastian.e.blanco@gmail.com>
2018-10-11 01:54:26 -0300

    add rules list to bar

--- client/src/components/LoggedNavbar.js
+++ client/src/components/LoggedNavbar.js
@@ -12,8 +12,11 @@ const LoggedNavbar = function () {
         <LinkContainer to="/app-server-status">
           <NavItem eventKey={2}>Estado de servidores</NavItem>
         </LinkContainer>
-        <LinkContainer to="/rules">
+        <LinkContainer to="/addRules">
           <NavItem eventKey={2}>Agregar reglas</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/rulesList">
+          <NavItem eventKey={2}>Lista de reglas</NavItem>
         </LinkContainer>
         <LinkContainer to="/logout">
           <NavItem eventKey={2}>Salir</NavItem>
