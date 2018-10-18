1fbd9f812ff3e398e31fafa89f386c3b03766b9a
sebastian <sebastian.e.blanco@gmail.com>
2018-10-18 14:34:43 -0300

    run script that create database table

--- database.sh
+++ database.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker exec -it $(docker ps | grep dev_web | awk '{ print $1 }') /bin/sh -c 'npm run db:create'
-docker exec -it $(docker ps | grep dev_web | awk '{ print $1 }') /bin/sh -c 'npm run db:migrate'
-docker exec -it $(docker ps | grep dev_web | awk '{ print $1 }') /bin/sh -c 'npm run model:create'
+docker exec -it $(docker ps | grep dev_web | awk '{ print $1 }') /bin/sh -c 'node scripts/db/liftDb.js'
