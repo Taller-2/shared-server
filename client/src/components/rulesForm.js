140f5b50459d9da19d723bd9e2cfb3c6af085948
sebastian <sebastian.e.blanco@gmail.com>
2018-10-25 13:00:20 -0300

    remove redundant title

--- client/src/components/rulesForm.js
+++ client/src/components/rulesForm.js
@@ -157,12 +157,6 @@ export default class RulesForm extends React.Component {
     return (
       <Grid>
         <Row className="show-grid">
-          <Col xs={12} md={6} mdOffset={3}>
-            <h1 style={{ textAlign: 'center' }} > Agregado de reglas para el calculo del costo de envio </h1>
-          </Col>
-        </Row>
-
-        <Row className="show-grid">
           <Col xs={12} md={4} mdOffset={9}>
             <Form horizontal>
               { this.listConditions() }
