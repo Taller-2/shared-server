b8b6186c1f6ee920b09829436b79ea0997a27e96
sebastian <sebastian.e.blanco@gmail.com>
2018-11-04 04:09:06 -0300

    add controller that requests the logged data to app server

--- server/controllers/app_server_logged_data.js
+++ server/controllers/app_server_logged_data.js
@@ -1,19 +1,22 @@
-const API_URL = process.env.APP_SERVER_ARTICLE_STATS || 'https://taller2-app-server.herokuapp.com/'
+const API_URL = 'https://taller2-app-server.herokuapp.com/'
 const HttpService = require('./http_service')
 const getAnalysis = require('./data_analysis').getAnalysis
 const httpStatus = require('http-status-codes')
 
-module.exports.makeAnalysis = function (req, res) {
+module.exports.makeAnalysis = function (req, res, next) {
+  // const loggedData = require('../../test/data_analysis_definitions').mixedArtcilesLoggedData
+  // const analysis = getAnalysis(loggedData)
+  // res.status(httpStatus.OK).json({ success: true, analysis: analysis })
   HttpService.get(API_URL + '/article_stats')
     .then(response => {
       // {"ok": True, "data": data}
       if (response.body.ok) {
-        res.status(httpStatus.OK)
-        res.send({ success: true, analysis: getAnalysis(req.body) })
+        res.status(httpStatus.OK).json({ success: true, analysis: getAnalysis(req.body) })
       } else {
-        res.status(httpStatus.BAD_REQUEST)
-        res.send({ success: false, error: 'response was not ok' })
+        res.status(httpStatus.BAD_REQUEST).json({ success: true, error: 'response was not ok' })
       }
     })
-    .catch(err => console.log('EEROR WHEN GETTING DATA FROM APP SERVER: ', err))
+    .catch(err => {
+      res.status(httpStatus.BAD_GATEWAY).json({ success: true, error: err })
+    })
 }
