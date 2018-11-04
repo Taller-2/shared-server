bae3667e5e30d3f228dfef542182d2e57a5dc6cd
sebastian <sebastian.e.blanco@gmail.com>
2018-11-04 04:03:49 -0300

    add test to posted, consulted, updated, deleted, and a mix of all articles

--- test/data_analysis_definitions.js
+++ test/data_analysis_definitions.js
@@ -1,4 +1,7 @@
-module.exports.loggedData = [
+// ----------------------------------------------------------------
+// POSTED
+// ----------------------------------------------------------------
+module.exports.postedArtcilesLoggedData = [
   {
     'name': 'IPhone1',
     'action': 'post',
@@ -61,11 +64,286 @@ module.exports.loggedData = [
   },
   {
     'name': 'Telecaster Fender',
-    'action': 'update',
+    'action': 'post',
     'description': 'A very good guitar',
     'price': 3000,
     'user': '1',
     'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  }
+]
+
+module.exports.postedDataAnalysis = {
+  'graph_1': {
+    xAxis: [
+      '2018-11-01T02:30:29.307Z',
+      '2017-11-01T02:30:29.307Z',
+      '2018-11-01T12:30:30.307Z',
+      '2017-11-01T15:30:29.307Z'
+    ],
+    yAxis: [3, 1, 2, 1],
+    title: 'Number of articles published as a function of time',
+    yAxisTitle: 'number of articles'
+  }
+}
+
+// ----------------------------------------------------------------
+// CONSULTED
+// ----------------------------------------------------------------
+module.exports.consultedArtcilesLoggedData = [
+  {
+    'name': 'IPhone1',
+    'action': 'get',
+    'description': 'A great cell phone',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'IPhone2',
+    'action': 'get',
+    'description': 'A not so great cell phone',
+    'price': 300,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'get',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  }
+]
+
+module.exports.consultedDataAnalysis = {
+  'graph_1': {
+    xAxis: [
+      '2018-11-01T02:30:29.307Z',
+      '2017-11-01T15:30:29.307Z'
+    ],
+    yAxis: [2, 1],
+    title: 'Number of articles consulted as a function of time',
+    yAxisTitle: 'number of articles'
+  }
+}
+
+// ----------------------------------------------------------------
+// DELETED
+// ----------------------------------------------------------------
+module.exports.deletedArtcilesLoggedData = [
+  {
+    'name': 'IPhone1',
+    'action': 'delete',
+    'description': 'A great cell phone',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'IPhone2',
+    'action': 'delete',
+    'description': 'A not so great cell phone',
+    'price': 300,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Sony ericsson',
+    'action': 'delete',
+    'description': 'A very old cell phone',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'delete',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'delete',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'delete',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  }
+]
+
+module.exports.deletedDataAnalysis = {
+  'graph_1': {
+    xAxis: [
+      '2018-11-01T02:30:29.307Z',
+      '2017-11-01T15:30:29.307Z'
+    ],
+    yAxis: [3, 3],
+    title: 'Number of articles deleted as a function of time',
+    yAxisTitle: 'number of articles'
+  }
+}
+
+// ----------------------------------------------------------------
+// UPDATED
+// ----------------------------------------------------------------
+module.exports.updatedArtcilesLoggedData = [
+  {
+    'name': 'IPhone1',
+    'action': 'update',
+    'description': 'A great cell phone',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'IPhone2',
+    'action': 'update',
+    'description': 'A not so great cell phone',
+    'price': 300,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'nokia 5200',
+    'action': 'update',
+    'description': 'An old cell phone',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'stratocaster Fender',
+    'action': 'update',
+    'description': 'A great guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T12:30:30.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'gibson E335',
+    'action': 'update',
+    'description': 'A very good guitar with incredible sound',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T12:30:30.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  }
+]
+
+module.exports.updatedDataAnalysis = {
+  'graph_1': {
+    xAxis: [
+      '2018-11-01T02:30:29.307Z',
+      '2017-11-01T02:30:29.307Z',
+      '2018-11-01T12:30:30.307Z'
+    ],
+    yAxis: [2, 1, 2],
+    title: 'Number of articles updated as a function of time',
+    yAxisTitle: 'number of articles'
+  }
+}
+
+// ----------------------------------------------------------------
+// MIXED
+// ----------------------------------------------------------------
+module.exports.mixedArtcilesLoggedData = [
+  {
+    'name': 'IPhone1',
+    'action': 'post',
+    'description': 'A great cell phone',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'IPhone2',
+    'action': 'post',
+    'description': 'A not so great cell phone',
+    'price': 300,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Sony ericsson',
+    'action': 'post',
+    'description': 'A very old cell phone',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'nokia 5200',
+    'action': 'post',
+    'description': 'An old cell phone',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T02:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'stratocaster Fender',
+    'action': 'post',
+    'description': 'A great guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T12:30:30.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'gibson E335',
+    'action': 'post',
+    'description': 'A very good guitar with incredible sound',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T12:30:30.307Z',
     'latitude': 37.658974,
     'longitude': -5.526148
   },
@@ -88,10 +366,130 @@ module.exports.loggedData = [
     'timestamp': '2017-11-01T15:30:29.307Z',
     'latitude': 37.658974,
     'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'get',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'get',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'get',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'update',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'update',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2018-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'update',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'update',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'delete',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'delete',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'delete',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T15:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'delete',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T16:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
+  },
+  {
+    'name': 'Telecaster Fender',
+    'action': 'delete',
+    'description': 'A very good guitar',
+    'price': 3000,
+    'user': '1',
+    'timestamp': '2017-11-01T17:30:29.307Z',
+    'latitude': 37.658974,
+    'longitude': -5.526148
   }
 ]
 
-module.exports.expectedDataAnalysis = {
+module.exports.mixedDataAnalysis = {
   'graph_1': {
     xAxis: [
       '2018-11-01T02:30:29.307Z',
@@ -101,6 +499,34 @@ module.exports.expectedDataAnalysis = {
     ],
     yAxis: [3, 1, 2, 1],
     title: 'Number of articles published as a function of time',
+    yAxisTitle: 'number of articles'
+  },
+  'graph_2': {
+    xAxis: [
+      '2017-11-01T15:30:29.307Z',
+      '2018-11-01T15:30:29.307Z'
+    ],
+    yAxis: [3, 1],
+    title: 'Number of articles consulted as a function of time',
+    yAxisTitle: 'number of articles'
+  },
+  'graph_3': {
+    xAxis: [
+      '2017-11-01T15:30:29.307Z',
+      '2017-11-01T16:30:29.307Z',
+      '2017-11-01T17:30:29.307Z'
+    ],
+    yAxis: [3, 1, 1],
+    title: 'Number of articles deleted as a function of time',
+    yAxisTitle: 'number of articles'
+  },
+  'graph_4': {
+    xAxis: [
+      '2018-11-01T15:30:29.307Z',
+      '2017-11-01T15:30:29.307Z'
+    ],
+    yAxis: [2, 2],
+    title: 'Number of articles updated as a function of time',
     yAxisTitle: 'number of articles'
   }
 }
--- test/data_analysis_test.js
+++ test/data_analysis_test.js
@@ -2,12 +2,48 @@ const chai = require('chai')
 chai.use(require('chai-http'))
 const should = require('should')
 const getAnalysis = require('../server/controllers/data_analysis').getAnalysis
-const { loggedData, expectedDataAnalysis } = require('./data_analysis_definitions')
+const {
+  postedArtcilesLoggedData,
+  postedDataAnalysis,
+  consultedArtcilesLoggedData,
+  consultedDataAnalysis,
+  updatedArtcilesLoggedData,
+  updatedDataAnalysis,
+  deletedArtcilesLoggedData,
+  deletedDataAnalysis,
+  mixedArtcilesLoggedData,
+  mixedDataAnalysis
+} = require('./data_analysis_definitions')
 
 describe('Data analysis test', function () {
-  it('should recieve analysis data from logged info', function (done) {
-    const analysis = getAnalysis(loggedData)
-    should.equal(JSON.stringify(analysis), JSON.stringify(expectedDataAnalysis))
+  it('should recieve published data analysis', function (done) {
+    const analysis = getAnalysis(postedArtcilesLoggedData)
+    should.equal(JSON.stringify(analysis), JSON.stringify(postedDataAnalysis))
+    done()
+  })
+  it('should recieve consulted data analysis', function (done) {
+    const analysis = getAnalysis(consultedArtcilesLoggedData)
+    should.equal(JSON.stringify(analysis), JSON.stringify(consultedDataAnalysis))
+    done()
+  })
+  it('should recieve updated data analysis', function (done) {
+    const analysis = getAnalysis(updatedArtcilesLoggedData)
+    should.equal(JSON.stringify(analysis), JSON.stringify(updatedDataAnalysis))
+    done()
+  })
+  it('should recieve deleted data analysis', function (done) {
+    const analysis = getAnalysis(deletedArtcilesLoggedData)
+    should.equal(JSON.stringify(analysis), JSON.stringify(deletedDataAnalysis))
+    done()
+  })
+  it('should recieve mixed data analysis', function (done) {
+    const analysis = getAnalysis(mixedArtcilesLoggedData)
+    should.equal(JSON.stringify(analysis), JSON.stringify(mixedDataAnalysis))
+    done()
+  })
+  it('should recieve empty analysis', function (done) {
+    const analysis = getAnalysis([])
+    should.equal(JSON.stringify(analysis), JSON.stringify({}))
     done()
   })
 })
