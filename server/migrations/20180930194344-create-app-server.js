e844352a1e23f57a97a7b32592b01fb7066c16c8
sebastian <sebastian.e.blanco@gmail.com>
2018-11-06 12:16:31 -0300

    add secret as a column

--- server/migrations/20180930194344-create-app-server.js
+++ server/migrations/20180930194344-create-app-server.js
@@ -12,6 +12,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
+      secret: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
       url: {
         allowNull: false,
         type: Sequelize.STRING
