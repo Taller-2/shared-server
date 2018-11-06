1d5d45429f0853cf3728af136557809f1aeb0bba
sebastian <sebastian.e.blanco@gmail.com>
2018-11-06 15:26:49 -0300

    test invalid update, create, get adding more tests.

--- test/user_test.js
+++ test/user_test.js
@@ -46,4 +46,96 @@ describe('User controller', function () {
           })
       })
   })
+  it('Deletes a user', (done) => {
+    model.User
+      .create({ name: 'old_name', email: 'old_email@example.com', password: 'example', enabled: 'true' })
+      .then(instance => {
+        chai
+          .request(server)
+          .delete(`${baseURL}/${instance.id}`)
+          .end((err, res) => {
+            should.equal(err, null)
+            res.should.have.status(httpStatus.CREATED)
+            res.body.success.should.be.equal(true)
+            done()
+          })
+      })
+  })
+  it('Deletes not existing user', (done) => {
+    chai
+      .request(server)
+      .delete(`${baseURL}/${'no valid'}`)
+      .end((err, res) => {
+        should.equal(err, null)
+        res.should.have.status(httpStatus.INTERNAL_SERVER_ERROR)
+        res.body.success.should.be.equal(false)
+        done()
+      })
+  })
+  it('get not existing user', (done) => {
+    chai
+      .request(server)
+      .get(`${baseURL}/${'no valid'}`)
+      .end((err, res) => {
+        should.equal(err, null)
+        res.should.have.status(httpStatus.INTERNAL_SERVER_ERROR)
+        res.body.success.should.be.equal(false)
+        done()
+      })
+  })
+  it('get existing user', (done) => {
+    model.User
+      .create({ name: 'old_name', email: 'old_email@example.com', password: 'example', enabled: 'true' })
+      .then(() => {
+        chai
+          .request(server)
+          .get(`${baseURL}`)
+          .end((err, res) => {
+            should.equal(err, null)
+            res.should.have.status(httpStatus.OK)
+            res.body.success.should.be.equal(true)
+            should.equal(res.body.users.length, 1)
+            done()
+          })
+      })
+  })
+  it('update existing user', (done) => {
+    const requestBody = { name: 'example', email: 'example@example.com' }
+    model.User
+      .create({ name: 'old_name', email: 'old_email@example.com', password: 'example', enabled: 'true' })
+      .then(instance => {
+        chai
+          .request(server)
+          .put(`${baseURL}/${'does not exist'}`)
+          .send(requestBody)
+          .end((err, res) => {
+            should.equal(err, null)
+            res.should.have.status(httpStatus.INTERNAL_SERVER_ERROR)
+            res.body.success.should.be.equal(false)
+            done()
+          })
+      })
+  })
+  it('email already registered', (done) => {
+    const requestBody = {
+      name: 'old_name',
+      email: 'old_email@example.com',
+      password: 'example',
+      enabled: 'true'
+    }
+    model.User
+      .create(requestBody)
+      .then(instance => {
+        chai
+          .request(server)
+          .post(baseURL)
+          .send(requestBody)
+          .end((err, res) => {
+            should.equal(err, null)
+            res.should.have.status(httpStatus.UNPROCESSABLE_ENTITY)
+            res.body.should.have.property('errors')
+            done()
+          })
+      })
+  })
 })
