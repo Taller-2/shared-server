33a66bc5ba99d4ff56b9585a8d374098f9bcd446
sebastian <sebastian.e.blanco@gmail.com>
2018-11-06 12:15:35 -0300

    remove timeout limit

--- test/app_server_test.js
+++ test/app_server_test.js
@@ -46,7 +46,6 @@ describe('App server controller', function () {
   })
 
   it('Lists all servers', (done) => {
-    this.timeout(2500)
     let instances = [
       { name: 'example1', url: 'example1.com' },
       { name: 'example2', url: 'example2.com' }
