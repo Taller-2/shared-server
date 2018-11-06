0e9bbfb30285f4964c691a7d727f896dba51f2a7
sebastian <sebastian.e.blanco@gmail.com>
2018-11-06 14:17:20 -0300

    remove conection function

--- test/shipment_cost_test.js
+++ test/shipment_cost_test.js
@@ -1,8 +1,7 @@
 const chai = require('chai')
 const should = require('should')
 const app = require('../server/index')
-const port = process.env.PORT || 5000
-let server = startConection(port, app)
+const server = app.listen()
 const truncate = require('../scripts/db/truncate')
 const httpStatus = require('http-status-codes')
 const {
@@ -14,19 +13,6 @@ const {
   discountRule
 } = require('./shipment_cost_definitions')
 chai.use(require('chai-http'))
-
-function startConection (port, app) {
-  let server = app.listen(port, 'localhost', () => {
-    console.log('++++++++++++++++++++++++++++++++++++++++++++++')
-    console.log("Calling app.listen's callback function.")
-    let host = server.address().address
-    let port = server.address().port
-    console.log('Example app listening at http://%s:%s', host, port)
-    console.log('enviroment: ', app.settings.env)
-    console.log('++++++++++++++++++++++++++++++++++++++++++++++')
-  })
-  return server
-}
 
 function ruleCheck (err, res, jsonRule) {
   should.equal(err, null)
