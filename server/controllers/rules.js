50eb7d07c7653a2d4f897cf57169affc2db2eecd
sebastian <sebastian.e.blanco@gmail.com>
2018-11-07 01:38:30 -0300

    embedding code by identing status and json response

--- server/controllers/rules.js
+++ server/controllers/rules.js
@@ -7,14 +7,20 @@ module.exports.findById = function (request, response) {
     .then(rules => {
       if (JSON.stringify(scope) === JSON.stringify({})) {
         // here we return all the rules in a vector
-        response.status(httpStatus.OK).json({ success: true, rules: rules })
+        response
+          .status(httpStatus.OK)
+          .json({ success: true, rules: rules })
       } else {
         // here we return only one rule
-        response.status(httpStatus.OK).json({ success: true, rules: rules[0] })
+        response
+          .status(httpStatus.OK)
+          .json({ success: true, rules: rules[0] })
       }
     })
     .catch(error => {
-      response.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: false, error: error })
+      response
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .json({ success: false, error: error })
     })
 }
 
@@ -43,37 +49,30 @@ module.exports.update = function (request, response) {
     { json: json },
     { where: { id: request.params.id } }
   )
-    .then(rule => response.status(httpStatus.CREATED).json({ success: true, rule: rule }))
+    .then(rule => {
+      response
+        .status(httpStatus.CREATED)
+        .json({ success: true, rule: rule })
+    })
     .catch(error => {
-      response.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: false, error: error })
+      response
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .json({ success: false, error: error })
     })
 }
 
 module.exports.delete = function (request, response) {
   model.Rules.destroy({ where: { id: request.params.id } })
-    .then(() => response.status(httpStatus.CREATED).json({ success: true }))
-    .catch(error => {
-      response.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: false, error: error })
+    .then(() => {
+      response
+        .status(httpStatus.CREATED)
+        .json({ success: true })
     })
-}
-
-module.exports.deleteAll = function (request, response) {
-  model.Rules.destroy({
-    where: {},
-    force: true
-  })
-    .then(() => response.status(httpStatus.CREATED).json({ success: true }))
     .catch(error => {
-      response.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: false, error: error })
+      response
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .json({ success: false, error: error })
     })
-}
-
-const { body } = require('express-validator/check')
-
-exports.validateCreate = () => {
-  return [
-    body('json', 'la regla es requerida').exists()
-  ]
 }
 
 const validationHandler = next => result => {
--- server/controllers/session.js
+++ server/controllers/session.js
@@ -7,14 +7,20 @@ const httpStatus = require('http-status-codes')
 function createSession (user, req, res) {
   if (user && bcrypt.compareSync(req.body.pass, user.password)) {
     const token = jwt.sign(user, secret)
-    res.status(httpStatus.OK).json({ message: 'Authenticated', token: token })
+    res
+      .status(httpStatus.OK)
+      .json({ message: 'Authenticated', token: token })
   } else {
-    res.status(httpStatus.UNAUTHORIZED).json({ message: 'Unauthorized' })
+    res
+      .status(httpStatus.UNAUTHORIZED)
+      .json({ message: 'Unauthorized' })
   }
 }
 
 function dbError (error, req, res) {
-  res.status(httpStatus.UNAUTHORIZED).json({ success: false, error: error })
+  res
+    .status(httpStatus.UNAUTHORIZED)
+    .json({ success: false, error: error })
 }
 
 module.exports.create = function (req, res, next) {
