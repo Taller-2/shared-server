2c318213c03eca999627cdf8ffa5ff8732e91f2a
sebastian <sebastian.e.blanco@gmail.com>
2018-11-08 02:23:11 -0300

    fix controller implementation and tests

--- test/shipments_test.js
+++ test/shipments_test.js
@@ -9,18 +9,25 @@ const truncate = require('../scripts/db/truncate')
 chai.use(require('chai-http'))
 
 describe('Shipments controller', function () {
-  beforeEach(async () => {
-    return truncate('Shipment').then(() => {
-      model.Shipment.create(dummyShipment)
+  beforeEach((done) => {
+    truncate('Shipment').then(() => {
+      model.Shipment
+        .create(dummyShipment)
+        .then(() => { done() })
     })
   })
 
   const baseURL = '/shipments'
-  const dummyShipment = { id: 1, transactionId: 123, address: 'Calle falsa 123', status: 'pending' }
+  const dummyShipment = {
+    id: 1,
+    transactionId: 123,
+    address: 'Calle falsa 123',
+    status: 'pending'
+  }
 
   it('Create shipment OK', (done) => {
     const other = Object.assign({}, dummyShipment)
-    other.transactionId = 111
+    other.id = 111
     chai
       .request(server)
       .post(baseURL)
@@ -30,9 +37,24 @@ describe('Shipments controller', function () {
         res.should.have.status(httpStatus.CREATED)
         const { success, shipment } = res.body
         success.should.be.equal(true)
-        shipment.transactionId.should.be.equal(other.transactionId)
+        shipment.id.should.be.equal(other.id)
         shipment.address.should.be.equal(other.address)
         shipment.status.should.be.equal(other.status)
+        done()
+      })
+  })
+  it('Create shipment FAIL transactionId too long', (done) => {
+    const other = Object.assign({}, dummyShipment)
+    other.transactionId = 9999999999999999999999999999999999999999999999999999999999999
+    chai
+      .request(server)
+      .post(baseURL)
+      .send(other)
+      .end((err, res) => {
+        should.equal(err, null)
+        res.should.have.status(httpStatus.INTERNAL_SERVER_ERROR)
+        const { success } = res.body
+        success.should.be.equal(false)
         done()
       })
   })
@@ -76,6 +98,35 @@ describe('Shipments controller', function () {
       })
   })
 
+  it('Update shipment FAIL invalid status type', (done) => {
+    const requestBody = { status: 1 }
+    chai.request(server)
+      .put(`${baseURL}/${dummyShipment.id}`)
+      .send(requestBody)
+      .end((err, res) => {
+        should.equal(err, null)
+        res.should.have.status(httpStatus.UNPROCESSABLE_ENTITY)
+        res.body.success.should.be.equal(false)
+        done()
+      })
+  })
+
+  it('Update shipment FAIL invalid id', (done) => {
+    const requestBody = { status: 'shipped' }
+    chai.request(server)
+      .put(`${baseURL}/${999}`)
+      .send(requestBody)
+      .end((err, res) => {
+        console.log('+++++++++++++++++++++++++++++++++++++++')
+        console.log('ERROR: ', res.text)
+        console.log('+++++++++++++++++++++++++++++++++++++++')
+        should.equal(err, null)
+        res.should.have.status(httpStatus.UNPROCESSABLE_ENTITY)
+        res.body.success.should.be.equal(false)
+        done()
+      })
+  })
+
   it('Get enums OK', (done) => {
     chai.request(server)
       .get(`${baseURL}/ui-enums`)
@@ -97,6 +148,16 @@ describe('Shipments controller', function () {
         chai.expect(res.body.shipments).to.be.an('array')
         chai.expect(res.body.shipments).to.have.length(1)
         chai.expect(res.body.shipments[0].id).to.equal(dummyShipment.id)
+        done()
+      })
+  })
+  it('delete shipments OK', (done) => {
+    chai.request(server)
+      .delete(`${baseURL}/${dummyShipment.id}`)
+      .end((err, res) => {
+        should.equal(err, null)
+        res.should.have.status(httpStatus.OK)
+        res.body.success.should.be.equal(true)
         done()
       })
   })
