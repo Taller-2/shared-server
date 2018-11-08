3d2a26430abb1c39de57c4aa28f84c58fa7c7956
sebastian <sebastian.e.blanco@gmail.com>
2018-11-07 23:01:18 -0300

    add implementation that checks if a rule is invalid

--- server/controllers/rules.js
+++ server/controllers/rules.js
@@ -19,22 +19,57 @@ module.exports.findById = function (request, response) {
 }
 
 module.exports.create = function (request, response, next) {
+  const { json: aRule } = request.body
+  const rule = JSON.parse(aRule)
+  if (!ruleIsValid(rule.conditions)) {
+    response
+      .status(httpStatus.BAD_REQUEST)
+      .json({ success: false, error: 'Missing conditions' })
+    return
+  }
+  model.Rules.create({
+    json: aRule,
+    enabled: false
+  })
+    .then(rule => {
+      response
+        .status(httpStatus.CREATED)
+        .json({ success: true, rule: rule })
+    })
+    .catch(error => {
+      response
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .json({ success: false, error: error })
+    })
+  /*
+  let clientError = false
   request.getValidationResult()
     .then(validationHandler())
     .then(() => {
       const { json: aRule } = request.body
+      const rule = JSON.parse(aRule)
+      if (!ruleIsValid(rule.conditions)) {
+        response
+          .status(httpStatus.BAD_REQUEST)
+          .json({ success: false, error: 'Missing conditions' })
+        clientError = true
+      }
+      if (clientError) return
       model.Rules.create({
         json: aRule,
         enabled: false
       })
         .then(rule => {
-          response.status(httpStatus.CREATED).json({ success: true, rule: rule })
+          response
+            .status(httpStatus.CREATED)
+            .json({ success: true, rule: rule })
         })
         .catch(error => {
           response.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: false, error: error })
         })
     })
     .catch(next)
+    */
 }
 
 module.exports.update = function (request, response) {
@@ -75,7 +110,7 @@ exports.validateCreate = () => {
     body('json', 'la regla es requerida').exists()
   ]
 }
-
+/*
 const validationHandler = next => result => {
   if (result.isEmpty()) return
   if (!next) {
@@ -83,4 +118,25 @@ const validationHandler = next => result => {
   } else {
     return next(new Error(JSON.stringify(result.array())))
   }
+}
+*/
+function isSingleConditionValid (singleCondition) {
+  if (singleCondition.value === null) return false
+  if (singleCondition.fact === null) return false
+  if (singleCondition.operator === null) return false
+  return true
+}
+
+const ruleIsValid = function (conditions) {
+  const booleanOp = Object.keys(conditions)[0]
+  if (booleanOp === 'all' || booleanOp === 'any') {
+    if (conditions[booleanOp].length === 0) return false
+    for (let aCondition of conditions[booleanOp]) {
+      if (!ruleIsValid(aCondition)) {
+        return false
+      }
+    }
+    return true
+  }
+  return isSingleConditionValid(conditions)
 }
