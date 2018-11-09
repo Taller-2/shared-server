96caf399e61e593cdf35721e30c03a1b3ec3afe3
sebastian <sebastian.e.blanco@gmail.com>
2018-11-09 14:42:07 -0300

    remove comment

--- server/routes/rules.js
+++ server/routes/rules.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-// const bodyParser = require('body-parser')
 const rulesController = require('../controllers/rules')
 const validation = require('../middlewares/validation')
 
