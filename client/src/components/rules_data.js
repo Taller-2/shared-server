35a91cadd482a9d754b8b67b51c595dca24846a8
sebastian <sebastian.e.blanco@gmail.com>
2018-11-10 01:59:36 -0300

    add shema of predefined operations, facts and types

---
+++ client/src/components/rules_data.js
@@ -0,0 +1,35 @@
+module.exports.facts = [
+  'daytrips',
+  'monthtrips',
+  'antiquity',
+  'userScore',
+  'paymentMethod',
+  'duration',
+  'distance',
+  'latitud',
+  'longitud',
+  'date',
+  'time',
+  'serverId',
+  'tripDate',
+  'tripTime',
+  'email',
+  'price'
+]
+module.exports.ops = [
+  'equal',
+  'greaterThanInclusive',
+  'lessThanInclusive',
+  'greaterThan',
+  'domainEqual',
+  'lessThan'
+]
+module.exports.type = [
+  'percentage',
+  'factor',
+  'sum',
+  'discount',
+  'surcharge',
+  'free',
+  'disabled'
+]
